82f6ec8587a49357fc30fff735f5d6001ce2a7cb
Lucas-Wenck <lucas.wenck@gmail.com>
2024-10-22 18:19:44 -0300

    add function to draw player sprite

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -12,6 +14,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen,(0,0,0))
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
