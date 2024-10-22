08b3b5281e9b46c016d28131ef07680f6e2156d3
Lucas-Wenck <lucas.wenck@gmail.com>
2024-10-22 19:38:29 -0300

    create groups and add player to them

--- main.py
+++ main.py
@@ -4,18 +4,27 @@ from player import *
 
 def main():
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     clock = pygame.time.Clock()
     dt = 0
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting asteroids!")
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        for updt in updatable:
+            updt.update(dt)
+        
         pygame.Surface.fill(screen,(0,0,0))
-        player.draw(screen)
-        player.update(dt)
+        
+        for drawing in drawable:
+            drawing.draw(screen)
+        
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
--- player.py
+++ player.py
@@ -1,3 +1,4 @@
+import pygame
 from circleshape import *
 from constants import *
 
@@ -15,7 +16,7 @@ class Player(CircleShape):
         return [a, b, c]
     
     def draw(self, screen):
-        player_spr = pygame.draw.polygon(screen, "white", self.triangle(), 2)
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
