01fc680b32686b658be035569f894695d6642033
Lucas-Wenck <lucas.wenck@gmail.com>
2024-10-22 23:47:44 -0300

    add shoot action for player

--- player.py
+++ player.py
@@ -1,8 +1,11 @@
 import pygame
 from circleshape import CircleShape
 from constants import *
+from shot import Shot
 
 class Player(CircleShape):
+    shoot_timer = 0
+
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
@@ -36,6 +39,17 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(-dt)
 
+        if keys[pygame.K_SPACE]:
+            if self.shoot_timer > 0:
+                self.shoot_timer -= dt
+            else:
+                self.shoot()
+                self.shoot_timer = PLAYER_SHOOT_COOLDOWN
+
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+    
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
