3a327ff2082ad3f7d80d46e2b800b60d053600a3
Lucas-Wenck <lucas.wenck@gmail.com>
2024-10-23 00:35:19 -0300

    add asteroid and shot collision and point system

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ from shot import Shot
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    font = pygame.font.Font(None, 36)
+    win_font = pygame.font.Font(None, 128)
+    point = 0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
@@ -22,26 +25,56 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     print("Starting asteroids!")
+    paused = False
+    check = False
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        for updt in updatable:
-            updt.update(dt)
+        if not paused:
+            for updt in updatable:
+                updt.update(dt)
 
-        for asteroid in asteroids:
-            if asteroid.collision(player):
-                print("Game over!")
-                sys.exit()
+            for asteroid in asteroids:
+                if asteroid.collision(player):
+                    print("Game over!")
+                    sys.exit()
+
+            for asteroid in asteroids:
+                for bullet in shots:
+                    if asteroid.collision(bullet):
+                        asteroid.kill()
+                        bullet.kill()
+                        point += 1
+            
+            pygame.Surface.fill(screen,(0,0,0))
+            
+            for drawing in drawable:
+                drawing.draw(screen)
+
+            x, y = 10, 10
+            text_surface = font.render(f"Current Score: {point}", True, pygame.Color('white'))
+            screen.blit(text_surface, (x, y))
+
+            if point == 1 and check == False:
+                paused = not paused
+            pygame.display.flip()
+            dt = clock.tick(60) / 1000
         
-        pygame.Surface.fill(screen,(0,0,0))
-        
-        for drawing in drawable:
-            drawing.draw(screen)
-        
-        pygame.display.flip()
-        dt = clock.tick(60) / 1000
+        else:
+            while paused:
+                for event in pygame.event.get():
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_e:
+                            paused = not paused
+                pygame.Surface.fill(screen,(0,0,0))
+                check = True
+                text_win = win_font.render(f"YOU WIN!!", True, pygame.Color('white'))
+                text_end = font.render(f"Please exit the game or press e to go into endless!", True, pygame.Color('white'))
+                screen.blit(text_win, (SCREEN_WIDTH/3, SCREEN_HEIGHT/2.2))
+                screen.blit(text_end, (x, y))
+                pygame.display.flip()
 
 if __name__ == "__main__":
     main()
